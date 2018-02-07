4484fd9c40e36d4d50ffeb06f43673d7dee48870
Luke Soenen <lukesoenen@gmail.com>
2018-02-06 22:09:00 -0500

    first commit

---
+++ lecture_10/lec_ex_10.py
@@ -0,0 +1,14 @@
+
+protected_url = 'https://api.twitter.com/1.1/account/settings.json'
+
+oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=resource_owner_key,
+                          resource_owner_secret=resource_owner_secret)
+r = oauth.get(protected_url)
+print (r.text)
+
+protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
+params = {'q':'food'}
+r = oauth.get(protected_url, params=params)
+print (r.text)
